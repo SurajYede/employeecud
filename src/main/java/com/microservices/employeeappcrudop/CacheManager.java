d3fd96194b025ec0ef31ed72cd16f03d0ead32a1
SurajYede <surajyede1212@gmail.com>
2022-11-28 21:30:14 +0530

    microservices -T 100

---
+++ src/main/java/com/microservices/employeeappcrudop/CacheManager.java
@@ -0,0 +1,26 @@
+package com.microservices.employeeappcrudop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+public class CacheManager {
+
+     @Autowired
+    EmployeeRepository employeeRepository;
+
+    public static HashMap<Integer, Employee> cache= new HashMap<>();
+
+    @Scheduled(cron = "* * * * * *")
+    public void loadCache()
+    {
+        System.out.println("Cache Loading started");
+        List<Employee> employeeList=employeeRepository.findAll();
+        employeeList.forEach(employee -> cache.put(employee.getEmpId(),employee));
+        System.out.println("Cache loading ended");
+    }
+}
